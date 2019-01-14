45dae3cff7b2b024ec4738023426adc960d97e1f
Michel Oosterhof <michel@oosterhof.net>
2019-01-15 00:29:04 +0400

    newtest

--- tests/test_default.py
+++ tests/test_default.py
@@ -7,11 +7,23 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-@pytest.mark.parametrize('svc', [
-  'cowrie'
-])
 def test_svc(host, svc):
-    service = host.service(svc)
-
+    service = host.service("cowrie")
     assert service.is_running
     assert service.is_enabled
+
+
+def test_cowrie_ssh_listen(host):
+    socket = host.socket('tcp://0.0.0.0:2222')
+    assert socket.is_listening
+
+
+def test_cowrie_telnet_listen(host):
+    socket = host.socket('tcp://0.0.0.0:2223')
+    assert socket.is_listening
+
+
+def test_cowrie_connection(host):
+    command = """echo test | nc root@localhost 2222"""
+    cmd = host.run(command)
+    assert 'Protocol mismatch' in cmd.stdout
